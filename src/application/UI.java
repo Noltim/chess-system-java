a734e73b1a087071f753002ce432850e97e58266
José Milton de Oliveira Neto <milton_oliveira18@live.com>
2023-04-20 10:45:30 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -28,6 +28,10 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen() {
+        for (int i = 0; i < 50; ++i) System.out.println();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
